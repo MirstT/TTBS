b6adbb49582176d935b8879ee7824c8f261c787a
MirstT <W_H_Norway@outlook.com>
2021-10-17 18:36:02 +0800

    完成17

--- script/20211017.js
+++ script/20211017.js
@@ -4,5 +4,19 @@
  * @Author: Mirst
  * @Date: 2021-10-17 18:28:13
  * @LastEditors: Mirst
- * @LastEditTime: 2021-10-17 18:28:14
+ * @LastEditTime: 2021-10-17 18:34:29
  */
+
+var swiper = new Swiper('.blog-slider',{
+    spaceBetween:30,
+    effect:'fade',
+    loop:true,
+    mousewheel:{
+        invert:false,
+    },
+
+    pagination:{
+        el:'.blog-slider_pagination',
+        clickable:true,
+    }
+});
