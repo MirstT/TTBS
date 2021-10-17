ecfad4260bee890e3b866ae87a44a3cdc2915f24
MirstT <W_H_Norway@outlook.com>
2021-10-17 18:56:21 +0800

    debug for 17

--- script/20211017.js
+++ script/20211017.js
@@ -4,19 +4,19 @@
  * @Author: Mirst
  * @Date: 2021-10-17 18:28:13
  * @LastEditors: Mirst
- * @LastEditTime: 2021-10-17 18:34:29
+ * @LastEditTime: 2021-10-17 18:28:14
  */
 
-var swiper = new Swiper('.blog-slider',{
-    spaceBetween:30,
-    effect:'fade',
-    loop:true,
-    mousewheel:{
-        invert:false,
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+        invert: false,
     },
 
-    pagination:{
-        el:'.blog-slider_pagination',
-        clickable:true,
+    pagination: {
+        el: '.blog-slider_pagination',
+        clickable: true,
     }
 });
